925b88f78ec21b83a58f9d2c360256b64430bfbb
V4run_Kum4r <69728949+varunkumar888@users.noreply.github.com>
2025-04-19 15:56:47 +0530

    Update tools-check.sh

--- tools-check.sh
+++ tools-check.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "[*] Checking for required tools..."
 
-tools=(nmap sqlmap nuclei subfinder httpx jq ffuf dalfox gau waybackurls)
+tools=(nmap sqlmap nuclei subfinder httpx jq ffuf dalfox gau waybackurls git curl)
 
 for tool in "${tools[@]}"; do
     if command -v $tool &>/dev/null; then
